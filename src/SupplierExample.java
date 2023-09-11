8c39999a9916b3390825d3962dcf7e6f2fa6923f
joaotorvitor <joaotorvitor1@gmail.com>
2023-09-11 20:37:35 -0300

    functional interfaces: supplier

---
+++ src/SupplierExample.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class SupplierExample {
+    public static void main(String[] args) {
+        Supplier<String> greeting = () -> "Hello, you're welcome";
+
+        List<String> greetingList = Stream.generate(greeting)
+                .limit(5)
+                .toList();
+
+        greetingList.forEach(System.out::println);
+    }
+}

f5959d9e47ab1c0eac2a6d09e57117e468f4bd81
joaotorvitor <joaotorvitor1@gmail.com>
2023-09-14 20:24:54 -0300

    functional interfaces: function<T,R>

---
+++ src/FunctionExample.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FunctionExample {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1,2,3,4,5);
+
+        Function<Integer, Integer> doubleNum = num -> num * 2;
+
+        List<Integer> doubledNums = nums.stream()
+                .map(doubleNum)
+                .collect(Collectors.toList()); // or just .toList();
+
+        List<Integer> doubledNums2 = nums.stream()
+                .map(
+//                    new Function<Integer, Integer>() {
+//                        @Override
+//                        public Integer apply(Integer num) {
+//                            return num * 2;
+//                        }
+//                    }
+                        num -> num * 2 // lambda
+                )
+                .collect(Collectors.toList()); // or just .toList();
+//        System.out.println(doubledNums);
+        doubledNums2.stream().forEach(System.out::println);
+    }
+}

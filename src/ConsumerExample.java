666a5aec630dee30ea070fd1e2106a7c7016ce5b
joaotorvitor <joaotorvitor1@gmail.com>
2023-09-14 20:39:49 -0300

    functional interfaces: predicate<T>

--- src/ConsumerExample.java
+++ src/ConsumerExample.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class ConsumerExample {
     public static void main(String[] args) {
 
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         // declaring a consumer
         Consumer<Integer> printEvenNumbers = number -> {
@@ -18,10 +19,9 @@ public class ConsumerExample {
         numbers.stream().forEach(printEvenNumbers);
 
         // printing odd numbers without declaring a consumer
-        numbers.stream().forEach(number -> {
-            if (number % 2 != 0) {
-                System.out.println(number);
-            }
-        });
+        // and using filter with predicate
+        numbers.stream()
+                .filter(number -> number % 2 == 1)
+                .forEach(System.out::println);
     }
 }
---
+++ src/PredicateExample.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class PredicateExample
+{
+    public static void main(String[] args)
+    {
+        List<String> words = Arrays.asList(
+                "java", "kotlin", "python", "javascript", "c", "go", "ruby");
+
+        words.stream()
+                .filter(w -> w.length() < 5)
+                .forEach(System.out::println);
+    }
+}
